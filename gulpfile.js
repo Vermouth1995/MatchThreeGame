2596a335770b7e5a3561331125f269541439871c
vermouth1995 <sixpenny1995@gmail.com>
2020-05-29 16:42:41 +0800

    🚚 remove code into src

--- gulpfile.js
+++ gulpfile.js
@@ -9,56 +9,52 @@ const browserify = require("gulp-browserify");
 const clean = require("gulp-clean-old");
 
 const cache_path = path.join(__dirname, "cache");
-const optput_path = path.join(__dirname, "dist");
-const resource_path = path.join(__dirname, "resource");
-const main_path = path.join(__dirname, "main");
+const output_path = path.join(__dirname, "dist");
+const resource_path = path.join(__dirname, "src/resource");
 
-const typescript_src = path.join(__dirname, "!(node_modules)/**/*.ts");
-const format_script_dist = path.join(__dirname, "./");
+const typescript_src = path.join(__dirname, "src/**/*.ts");
+const format_script_dist = path.join(__dirname, "src");
 
 const build_src_html = path.join(__dirname, "cache/main/html/match_three_game.js");
 const build_dist_html = path.join(__dirname, "dist/main/html");
 
-const copy_src_html = path.join(__dirname, "main/html/MatchThreeGame.html");
+const copy_src_html = path.join(__dirname, "src/main/html/MatchThreeGame.html");
 const copy_dist_html = build_dist_html;
 
 const copy_src_resource = path.join(resource_path, "**/*");
-const copy_dist_resource = path.join(optput_path, "resource");
+const copy_dist_resource = path.join(output_path, "resource");
 
-gulp.task("format", function() {
-	return gulp
-		.src(typescript_src)
-		.pipe(pretty(prettier_config))
-		.pipe(gulp.dest(format_script_dist));
+gulp.task("format", function () {
+	return gulp.src(typescript_src).pipe(pretty(prettier_config)).pipe(gulp.dest(format_script_dist));
 });
 
-gulp.task("copy_html", function() {
+gulp.task("copy_html", function () {
 	return gulp.src(copy_src_html, {}).pipe(gulp.dest(copy_dist_html));
 });
 
-gulp.task("copy_resource", function() {
+gulp.task("copy_resource", function () {
 	return gulp.src(copy_src_resource, {}).pipe(gulp.dest(copy_dist_resource));
 });
 
 gulp.task("copy", gulp.parallel("copy_html", "copy_resource"));
 
-gulp.task("clean_cache", function() {
+gulp.task("clean_cache", function () {
 	return gulp.src(cache_path + "/*", { read: false }).pipe(clean());
 });
-gulp.task("clean_dist", function() {
-	return gulp.src(optput_path + "/*", { read: false }).pipe(clean());
+gulp.task("clean_dist", function () {
+	return gulp.src(output_path + "/*", { read: false }).pipe(clean());
 });
 
 gulp.task("clean", gulp.parallel("clean_cache", "clean_dist"));
 
-gulp.task("cmd_complied", function() {
+gulp.task("cmd_complied", function () {
 	return gulp
 		.src(typescript_src)
 		.pipe(ts({ noImplicitAny: true, module: "commonjs" }))
 		.pipe(gulp.dest(cache_path));
 });
 
-gulp.task("cmd_link_html", function() {
+gulp.task("cmd_link_html", function () {
 	return gulp
 		.src(build_src_html)
 		.pipe(browserify({ insertGlobals: false }))
